c747c7b9d8ac1e3f24ed218c03d0a6a8d5a64337
Ross <qinruoxi@outlook.com>
2025-09-06 13:14:19 -0700

    Implement RMSNorm and passed tests.

--- cs336_basics/basic_modules.py
+++ cs336_basics/basic_modules.py
@@ -2,6 +2,7 @@ import math
 import torch
 from torch import nn
 from einops import einsum
+from jaxtyping import Float, Int
 
 
 class Linear(nn.Module):
@@ -15,7 +16,7 @@ class Linear(nn.Module):
         nn.init.trunc_normal_(self.weight, mean=0, std=std,
                               a=-3.0 * std, b=3.0 * std)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Float[torch.Tensor, "... d_in"]) -> Float[torch.Tensor, "... d_out"]:
         return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
 
 
@@ -26,5 +27,23 @@ class Embedding(nn.Module):
             num_embeddings, embedding_dim, dtype=dtype, device=device))
         nn.init.trunc_normal_(self.weight, 0, 1, -3, 3)
 
-    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+    def forward(self, token_ids: Int[torch.Tensor, "... seq_len"]) -> Float[torch.Tensor, "... seq_len d_model"]:
         return self.weight[token_ids]
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.gain = nn.Parameter(torch.ones(
+            d_model, dtype=dtype, device=device))
+
+    def forward(self, x: Float[torch.Tensor, "... d_model"]) -> Float[torch.Tensor, "... d_model"]:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        rms: Float[torch.Tensor, "... one"] = torch.rsqrt(torch.sum(x**2, -1, keepdim=True) /
+                                                          self.d_model + self.eps)
+        result = einsum(x, self.gain, rms,
+                        "... d_model, d_model, ... one -> ... d_model")
+        return result.to(in_dtype)
